293781db94353381dc12f1904ceb84dd03d3ebc6
oesteban <code@oscaresteban.es>
2017-09-11 15:52:28 -0700

    add logging to shell runner

--- launcher/utils/shell.py
+++ launcher/utils/shell.py
@@ -8,10 +8,38 @@
 Running bash commands
 """
 
-def bash(cmd):
+
+def bash(cmd, task_id=0, job_id=0):
     """
     A task runner for bash
     """
+    import os
+    from datetime import datetime
     import subprocess
-    pout = abs(subprocess.run(cmd, shell=True).returncode)
+
+    log_path = os.path.join(
+        os.path.abspath('.dlauncher-rmi-%s' % job_id), 'task-%05d')
+    out_file = open((log_path + '.out') % task_id, 'w')
+    err_file = open((log_path + '.err') % task_id, 'w')
+
+    out_file.write("""\
+[{date}] Starting task {job_id}-{task_id}:
+{cmd}
+""".format(
+        date=datetime.now().isoformat(timespec='seconds'),
+        task_id=task_id,
+        job_id=job_id,
+        cmd=cmd
+    ))
+    out_file.flush()
+    task = subprocess.run(cmd, shell=True, stdout=out_file, stderr=err_file)
+    pout = abs(task.returncode)
+
+    out_file.write("""[{date}] Finished task {job_id}-{task_id}.""".format(
+        date=datetime.now().isoformat(timespec='seconds'),
+        task_id=task_id,
+        job_id=job_id
+    ))
+    out_file.close()
+    err_file.close()
     return pout
